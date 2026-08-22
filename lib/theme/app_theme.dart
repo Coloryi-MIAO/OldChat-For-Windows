a1f3997d9b842ef5a27ddbfc3d209b9f897b92ab
Coloryi-MIAO <Shiroko_white@outlook.com>
2026-08-22 07:55:28 +0000

    fix: use Flutter 3.19 card theme API

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -69,7 +69,7 @@ class AppTheme {
         elevation: 0,
         centerTitle: false,
       ),
-      cardTheme: CardThemeData(
+      cardTheme: CardTheme(
         color: surface,
         elevation: 0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
